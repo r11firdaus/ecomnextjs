63765d1738d479ed138b52cb6da3ae4e293ac359
Reza Firdaus <rezafirdaus@Rezas-MacBook-Pro.local>
2021-05-27 09:11:19 +0700

    set socket for production

--- function/socket.js
+++ function/socket.js
@@ -1,3 +1,3 @@
-import io from "socket.io-client";
-export const socket = io("http://localhost:3001")
-// export const socket = io("https://jwallin.herokuapp.com/");
+import io from "socket.io-client";q
+// export const socket = io("http://localhost:3001")
+export const socket = io("https://jwallin.herokuapp.com/")
